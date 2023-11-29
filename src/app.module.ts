f1afd353cc75d0990efab9337776776617bcbb7f
AdrianLopez-55 <adriplev@gmail.com>
2023-11-29 11:29:21 -0400

    organigrama v2

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { DepartmentsModule } from './departments/departments.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1/', {
+      dbName: 'organigrama-v2',
+    }),
+    DepartmentsModule,
+    HttpModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
